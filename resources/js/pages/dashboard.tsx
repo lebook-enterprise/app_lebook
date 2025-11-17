9d84a942b99beab6123246cbf132aeb047e242f6
khoime <j.m.tejada@hotmail.com>
2025-11-17 11:28:11 -0500

    preliminary things

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -3,6 +3,8 @@ import AppLayout from '@/layouts/app-layout';
 import { dashboard } from '@/routes';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import { useState } from 'react';
+import {data} from './data.js';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -11,24 +13,33 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+console.log(data)
+
 export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
-                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
-                    </div>
+                <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border h-10">
+                    <form action="" method="POST">
+                        <input
+                            onChange={(e) => setSearch(e.target.value)}
+                            type='text'
+                            className="m-2 w-100"
+                            placeholder="Search products"
+                        />
+                    </form>
                 </div>
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                    <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                <div className="grid auto-rows-min gap-4 md:grid-cols-2">
+                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        <h1> history alike feed of the actions</h1>
+                        <h1> Or products. just to search</h1>
+                    </div>
+                    <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
+                        <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
+                        <h1> Pending, lost or in  revision matters</h1>
+                    </div>
                 </div>
             </div>
         </AppLayout>
---
+++ resources/js/pages/data.js
@@ -0,0 +1,701 @@
+export const data = [{
+    "id": 1,
+    "name": "Fresh Cilantro",
+    "barcode": "011307116",
+    "category_id": 36,
+    "location": "SQ1339",
+    "status": "pretium"
+}, {
+    "id": 2,
+    "name": "Solar String Lights",
+    "barcode": "064200765",
+    "category_id": 47,
+    "location": "AZ6282",
+    "status": "a"
+}, {
+    "id": 3,
+    "name": "Magnetic Puzzle Board",
+    "barcode": "064107994",
+    "category_id": 12,
+    "location": "TG3677",
+    "status": "et"
+}, {
+    "id": 4,
+    "name": "Wall-Mounted Bookshelf",
+    "barcode": "056003174",
+    "category_id": 1,
+    "location": "WN9691",
+    "status": "lacinia"
+}, {
+    "id": 5,
+    "name": "Barbecue Chicken Pizza",
+    "barcode": "084201278",
+    "category_id": 72,
+    "location": "AI2869",
+    "status": "nulla"
+}, {
+    "id": 6,
+    "name": "Spicy Tuna Rolls",
+    "barcode": "064203021",
+    "category_id": 23,
+    "location": "NH5575",
+    "status": "at"
+}, {
+    "id": 7,
+    "name": "Luxe Velvet Blazer",
+    "barcode": "053107989",
+    "category_id": 5,
+    "location": "KL3479",
+    "status": "dictumst"
+}, {
+    "id": 8,
+    "name": "Foot Massager",
+    "barcode": "084201171",
+    "category_id": 48,
+    "location": "NZ9369",
+    "status": "erat"
+}, {
+    "id": 9,
+    "name": "Gluten-Free Biscuits",
+    "barcode": "322275487",
+    "category_id": 22,
+    "location": "KE4306",
+    "status": "ultrices"
+}, {
+    "id": 10,
+    "name": "Foam Building Blocks for Kids",
+    "barcode": "026008811",
+    "category_id": 5,
+    "location": "SA8035",
+    "status": "nisi"
+}, {
+    "id": 11,
+    "name": "Vegan Chocolate Cake Mix",
+    "barcode": "101105817",
+    "category_id": 77,
+    "location": "AF7295",
+    "status": "eget"
+}, {
+    "id": 12,
+    "name": "Stuffed Grape Leaves",
+    "barcode": "274970830",
+    "category_id": 5,
+    "location": "LH7416",
+    "status": "eros"
+}, {
+    "id": 13,
+    "name": "Folding Exercise Bike",
+    "barcode": "111316612",
+    "category_id": 28,
+    "location": "QR3757",
+    "status": "fringilla"
+}, {
+    "id": 14,
+    "name": "Raspberry Lemonade Mix",
+    "barcode": "271974295",
+    "category_id": 43,
+    "location": "TG9254",
+    "status": "vestibulum"
+}, {
+    "id": 15,
+    "name": "Thai Red Curry Paste",
+    "barcode": "011402024",
+    "category_id": 3,
+    "location": "AV9918",
+    "status": "porttitor"
+}, {
+    "id": 16,
+    "name": "Maple Chipotle Glaze",
+    "barcode": "051403630",
+    "category_id": 24,
+    "location": "SA3103",
+    "status": "nulla"
+}, {
+    "id": 17,
+    "name": "Ginger Turmeric Latte Mix",
+    "barcode": "051400507",
+    "category_id": 23,
+    "location": "AC7293",
+    "status": "nibh"
+}, {
+    "id": 18,
+    "name": "Personal Blender with Travel Cup",
+    "barcode": "041200775",
+    "category_id": 27,
+    "location": "AZ9583",
+    "status": "risus"
+}, {
+    "id": 19,
+    "name": "Portable Blender",
+    "barcode": "042204932",
+    "category_id": 43,
+    "location": "WN2810",
+    "status": "amet"
+}, {
+    "id": 20,
+    "name": "Black Bean & Corn Salad",
+    "barcode": "264071590",
+    "category_id": 10,
+    "location": "UA9176",
+    "status": "nibh"
+}, {
+    "id": 21,
+    "name": "Elderberry Syrup",
+    "barcode": "103101589",
+    "category_id": 4,
+    "location": "TG7872",
+    "status": "maecenas"
+}, {
+    "id": 22,
+    "name": "Spicy Beef Taco Mix",
+    "barcode": "072412354",
+    "category_id": 36,
+    "location": "WN2179",
+    "status": "fusce"
+}, {
+    "id": 23,
+    "name": "Margherita Pizza",
+    "barcode": "062205034",
+    "category_id": 55,
+    "location": "WN2925",
+    "status": "hac"
+}, {
+    "id": 24,
+    "name": "Wooden Children's Play Kitchen",
+    "barcode": "031902766",
+    "category_id": 12,
+    "location": "NZ3206",
+    "status": "pellentesque"
+}, {
+    "id": 25,
+    "name": "Portable Ice Maker",
+    "barcode": "104104750",
+    "category_id": 27,
+    "location": "DL6412",
+    "status": "tincidunt"
+}, {
+    "id": 26,
+    "name": "Children's Science Experiment Lab Kit",
+    "barcode": "071900948",
+    "category_id": 10,
+    "location": "AZ1863",
+    "status": "risus"
+}, {
+    "id": 27,
+    "name": "Chia Seed Pudding",
+    "barcode": "082000109",
+    "category_id": 3,
+    "location": "AV3620",
+    "status": "enim"
+}, {
+    "id": 28,
+    "name": "Almond Flour Pizza Crust",
+    "barcode": "321070007",
+    "category_id": 12,
+    "location": "AM9111",
+    "status": "blandit"
+}, {
+    "id": 29,
+    "name": "Vegetable and Cheese Quesadillas",
+    "barcode": "107002192",
+    "category_id": 40,
+    "location": "AI8523",
+    "status": "nec"
+}, {
+    "id": 30,
+    "name": "Sriracha Sauce",
+    "barcode": "072413706",
+    "category_id": 98,
+    "location": "AM9767",
+    "status": "sapien"
+}, {
+    "id": 31,
+    "name": "Herbed Couscous",
+    "barcode": "075908195",
+    "category_id": 33,
+    "location": "KE7714",
+    "status": "non"
+}, {
+    "id": 32,
+    "name": "Cheesy Broccoli Soup Mix",
+    "barcode": "064101398",
+    "category_id": 86,
+    "location": "BA7629",
+    "status": "pede"
+}, {
+    "id": 33,
+    "name": "Collapsible Colander",
+    "barcode": "084201171",
+    "category_id": 78,
+    "location": "NH4271",
+    "status": "sapien"
+}, {
+    "id": 34,
+    "name": "Wrap Front Midi Skirt",
+    "barcode": "101202257",
+    "category_id": 3,
+    "location": "UA6826",
+    "status": "dictumst"
+}, {
+    "id": 35,
+    "name": "Kale and Quinoa Salad",
+    "barcode": "062106036",
+    "category_id": 84,
+    "location": "AF9091",
+    "status": "donec"
+}, {
+    "id": 36,
+    "name": "Lightweight Workout Tank",
+    "barcode": "091016498",
+    "category_id": 81,
+    "location": "LH1606",
+    "status": "hac"
+}, {
+    "id": 37,
+    "name": "Personalized Pet ID Tags",
+    "barcode": "307074098",
+    "category_id": 13,
+    "location": "TG2803",
+    "status": "lobortis"
+}, {
+    "id": 38,
+    "name": "Portable Hand Warmer",
+    "barcode": "114922443",
+    "category_id": 32,
+    "location": "AC8669",
+    "status": "sagittis"
+}, {
+    "id": 39,
+    "name": "Ice Pack",
+    "barcode": "072413777",
+    "category_id": 53,
+    "location": "AV1831",
+    "status": "erat"
+}, {
+    "id": 40,
+    "name": "Pumpkin Pie Spice",
+    "barcode": "071924458",
+    "category_id": 57,
+    "location": "BA4494",
+    "status": "at"
+}, {
+    "id": 41,
+    "name": "Biodegradable Dog Waste Bags",
+    "barcode": "113124637",
+    "category_id": 91,
+    "location": "SK1004",
+    "status": "aliquam"
+}, {
+    "id": 42,
+    "name": "Oatmeal Raisin Cookies",
+    "barcode": "271070801",
+    "category_id": 1,
+    "location": "DL1765",
+    "status": "ullamcorper"
+}, {
+    "id": 43,
+    "name": "Linen Trousers",
+    "barcode": "242074425",
+    "category_id": 81,
+    "location": "EK2864",
+    "status": "aliquam"
+}, {
+    "id": 44,
+    "name": "Portable Phone Mug Holder",
+    "barcode": "071214249",
+    "category_id": 11,
+    "location": "QR6930",
+    "status": "eget"
+}, {
+    "id": 45,
+    "name": "Peas (frozen)",
+    "barcode": "113000861",
+    "category_id": 77,
+    "location": "AA7291",
+    "status": "iaculis"
+}, {
+    "id": 46,
+    "name": "Comfortable Jogger Pants",
+    "barcode": "054000807",
+    "category_id": 67,
+    "location": "AM3516",
+    "status": "sit"
+}, {
+    "id": 47,
+    "name": "Scented Soy Candles",
+    "barcode": "107006512",
+    "category_id": 1,
+    "location": "QR6999",
+    "status": "nam"
+}, {
+    "id": 48,
+    "name": "Slim Wallet",
+    "barcode": "063013924",
+    "category_id": 40,
+    "location": "AF5854",
+    "status": "duis"
+}, {
+    "id": 49,
+    "name": "Winter Insulated Jacket",
+    "barcode": "101112716",
+    "category_id": 81,
+    "location": "AF3394",
+    "status": "pellentesque"
+}, {
+    "id": 50,
+    "name": "Vegetable Curry",
+    "barcode": "103102889",
+    "category_id": 32,
+    "location": "KL3236",
+    "status": "pede"
+}, {
+    "id": 51,
+    "name": "Slingback Sandals",
+    "barcode": "041000124",
+    "category_id": 58,
+    "location": "AC9922",
+    "status": "odio"
+}, {
+    "id": 52,
+    "name": "Coconut Chips",
+    "barcode": "124303007",
+    "category_id": 40,
+    "location": "KE9869",
+    "status": "suspendisse"
+}, {
+    "id": 53,
+    "name": "Herb Drying Rack",
+    "barcode": "083902387",
+    "category_id": 40,
+    "location": "SA9026",
+    "status": "neque"
+}, {
+    "id": 54,
+    "name": "Set of Silicone Baking Molds",
+    "barcode": "271972844",
+    "category_id": 7,
+    "location": "EK6905",
+    "status": "nunc"
+}, {
+    "id": 55,
+    "name": "Free-Range Eggs",
+    "barcode": "061101197",
+    "category_id": 70,
+    "location": "SQ4857",
+    "status": "mus"
+}, {
+    "id": 56,
+    "name": "Electric Heat Pad",
+    "barcode": "053202208",
+    "category_id": 94,
+    "location": "ET2047",
+    "status": "blandit"
+}, {
+    "id": 57,
+    "name": "Organic Lentil Soup",
+    "barcode": "063114276",
+    "category_id": 94,
+    "location": "NH4271",
+    "status": "odio"
+}, {
+    "id": 58,
+    "name": "Home Cleaning Robot",
+    "barcode": "111907924",
+    "category_id": 52,
+    "location": "KL3966",
+    "status": "pharetra"
+}, {
+    "id": 59,
+    "name": "Art Supplies Organizer",
+    "barcode": "081025208",
+    "category_id": 64,
+    "location": "BA9697",
+    "status": "ac"
+}, {
+    "id": 60,
+    "name": "Cinnamon Raisin Bagels",
+    "barcode": "111322813",
+    "category_id": 11,
+    "location": "KE2522",
+    "status": "varius"
+}, {
+    "id": 61,
+    "name": "Scented Candle Set",
+    "barcode": "102303171",
+    "category_id": 64,
+    "location": "SQ3555",
+    "status": "sit"
+}, {
+    "id": 62,
+    "name": "Chickpea Salad",
+    "barcode": "065000171",
+    "category_id": 28,
+    "location": "QF1039",
+    "status": "velit"
+}, {
+    "id": 63,
+    "name": "Fleece Lined Leggings",
+    "barcode": "021201639",
+    "category_id": 85,
+    "location": "AA5996",
+    "status": "ante"
+}, {
+    "id": 64,
+    "name": "Mediterranean Couscous",
+    "barcode": "102301092",
+    "category_id": 93,
+    "location": "EK8895",
+    "status": "aliquet"
+}, {
+    "id": 65,
+    "name": "Oven Thermometer",
+    "barcode": "064109264",
+    "category_id": 80,
+    "location": "KL9083",
+    "status": "duis"
+}, {
+    "id": 66,
+    "name": "Herbal Garden Starter Kit",
+    "barcode": "253176215",
+    "category_id": 89,
+    "location": "QF6015",
+    "status": "blandit"
+}, {
+    "id": 67,
+    "name": "Elegant Maxi Skirt",
+    "barcode": "112002080",
+    "category_id": 78,
+    "location": "SK9204",
+    "status": "nibh"
+}, {
+    "id": 68,
+    "name": "Tomato Basil Soup",
+    "barcode": "111308442",
+    "category_id": 72,
+    "location": "SQ7691",
+    "status": "blandit"
+}, {
+    "id": 69,
+    "name": "Camping Tarp",
+    "barcode": "063101399",
+    "category_id": 48,
+    "location": "AA3540",
+    "status": "sapien"
+}, {
+    "id": 70,
+    "name": "Coconut Rice",
+    "barcode": "101205063",
+    "category_id": 68,
+    "location": "ET9389",
+    "status": "rhoncus"
+}, {
+    "id": 71,
+    "name": "Gingerbread House Kit",
+    "barcode": "061212086",
+    "category_id": 83,
+    "location": "LH9879",
+    "status": "sed"
+}, {
+    "id": 72,
+    "name": "Mini Meatballs",
+    "barcode": "053202130",
+    "category_id": 15,
+    "location": "LA2996",
+    "status": "cras"
+}, {
+    "id": 73,
+    "name": "Kids' Gardening Tools Set",
+    "barcode": "124002942",
+    "category_id": 17,
+    "location": "TG6062",
+    "status": "faucibus"
+}, {
+    "id": 74,
+    "name": "Golf Putting Green",
+    "barcode": "101107802",
+    "category_id": 93,
+    "location": "CX7264",
+    "status": "lacus"
+}, {
+    "id": 75,
+    "name": "LED Desk Light",
+    "barcode": "081000728",
+    "category_id": 80,
+    "location": "IB5135",
+    "status": "orci"
+}, {
+    "id": 76,
+    "name": "Sweet Potato Fries",
+    "barcode": "102105146",
+    "category_id": 83,
+    "location": "IB9152",
+    "status": "eget"
+}, {
+    "id": 77,
+    "name": "Mediterranean Couscous",
+    "barcode": "102101519",
+    "category_id": 77,
+    "location": "WN7709",
+    "status": "odio"
+}, {
+    "id": 78,
+    "name": "Cranberry Almond Granola",
+    "barcode": "064005203",
+    "category_id": 26,
+    "location": "EK8650",
+    "status": "ut"
+}, {
+    "id": 79,
+    "name": "Pet Hair Removal Brush",
+    "barcode": "221272361",
+    "category_id": 92,
+    "location": "LA9660",
+    "status": "dui"
+}, {
+    "id": 80,
+    "name": "Italian Meatballs",
+    "barcode": "111316612",
+    "category_id": 16,
+    "location": "ET9826",
+    "status": "mattis"
+}, {
+    "id": 81,
+    "name": "Car Trash Can",
+    "barcode": "061102549",
+    "category_id": 32,
+    "location": "KE1327",
+    "status": "dui"
+}, {
+    "id": 82,
+    "name": "Pasta Maker Machine",
+    "barcode": "065405491",
+    "category_id": 93,
+    "location": "AZ1674",
+    "status": "non"
+}, {
+    "id": 83,
+    "name": "Miso Soup Mix",
+    "barcode": "031101211",
+    "category_id": 51,
+    "location": "LH1504",
+    "status": "semper"
+}, {
+    "id": 84,
+    "name": "Sweet Potato Chips",
+    "barcode": "091303376",
+    "category_id": 60,
+    "location": "NH1524",
+    "status": "ante"
+}, {
+    "id": 85,
+    "name": "Pet Hair Vacuum Attachment",
+    "barcode": "221971293",
+    "category_id": 70,
+    "location": "AC3505",
+    "status": "iaculis"
+}, {
+    "id": 86,
+    "name": "High-Low Hem Tee",
+    "barcode": "072401048",
+    "category_id": 5,
+    "location": "TG5603",
+    "status": "pulvinar"
+}, {
+    "id": 87,
+    "name": "Curried Lentil Salad",
+    "barcode": "061204900",
+    "category_id": 1,
+    "location": "ET7205",
+    "status": "dui"
+}, {
+    "id": 88,
+    "name": "Weighted Jump Rope with Counter",
+    "barcode": "053274223",
+    "category_id": 69,
+    "location": "AI8851",
+    "status": "pede"
+}, {
+    "id": 89,
+    "name": "Feta Cheese",
+    "barcode": "021310591",
+    "category_id": 47,
+    "location": "UA9709",
+    "status": "ipsum"
+}, {
+    "id": 90,
+    "name": "Dish Rack",
+    "barcode": "091403548",
+    "category_id": 60,
+    "location": "BA1442",
+    "status": "quam"
+}, {
+    "id": 91,
+    "name": "Fresh Basil Pesto",
+    "barcode": "284170468",
+    "category_id": 22,
+    "location": "WN1641",
+    "status": "ut"
+}, {
+    "id": 92,
+    "name": "Photo Album",
+    "barcode": "114926009",
+    "category_id": 72,
+    "location": "BA2034",
+    "status": "pede"
+}, {
+    "id": 93,
+    "name": "Sriracha Honey Glaze",
+    "barcode": "021200575",
+    "category_id": 51,
+    "location": "NH4808",
+    "status": "sapien"
+}, {
+    "id": 94,
+    "name": "Set of Gardening Gloves with Claws",
+    "barcode": "011401148",
+    "category_id": 23,
+    "location": "AZ6261",
+    "status": "elementum"
+}, {
+    "id": 95,
+    "name": "Gardening Fairy Figurines",
+    "barcode": "042104469",
+    "category_id": 75,
+    "location": "SA4548",
+    "status": "amet"
+}, {
+    "id": 96,
+    "name": "Creamy Tomato Basil Soup",
+    "barcode": "067010583",
+    "category_id": 67,
+    "location": "WN2781",
+    "status": "lobortis"
+}, {
+    "id": 97,
+    "name": "Cocktail Shaker and Mixing Glass Set",
+    "barcode": "113017870",
+    "category_id": 26,
+    "location": "AV2656",
+    "status": "justo"
+}, {
+    "id": 98,
+    "name": "Adjustable Dog Harness",
+    "barcode": "111104853",
+    "category_id": 93,
+    "location": "NZ5754",
+    "status": "faucibus"
+}, {
+    "id": 99,
+    "name": "Handheld Vacuum",
+    "barcode": "082901677",
+    "category_id": 57,
+    "location": "AZ3825",
+    "status": "morbi"
+}, {
+    "id": 100,
+    "name": "Chocolate Almond Milk",
+    "barcode": "071926508",
+    "category_id": 38,
+    "location": "IB8450",
+    "status": "convallis"
+}]
