3cd91aa0ad288b5736fab7636290888891d07c2f
khoime <j.m.tejada@hotmail.com>
2025-11-17 20:32:47 -0500

    feat: product models praying that it works

---
+++ app/Models/Product.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Validation\ValidationException;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'category_id',
+        'name',
+        'sku',
+        'description',
+    ];
+
+    protected static function booted()
+    {
+        static::creating(
+            function ($product) {
+                if(empty($product->sku)) {
+                    $product->sku = self::generateSku($product);
+                }
+            }
+        );
+
+        static::created(function(Product $product)
+        {
+            $product->stock()->create(['stock' => 0]);
+        });
+    }
+
+    public static function generateSku($product) {
+        $prefix = strtoupper(substr($product->name, 0, 3));
+        $unique = strtoupper(Str::random(4));
+        $lastId = static::query()->max('id') ?? 0;
+        $idPart = str_pad($lastId + 1, 6, '0', STR_PAD_LEFT);
+
+        return "$prefix-$idPart-$unique";
+    }
+
+    public function stock()
+    {
+        return $this->hasOne(ProductStock::class);
+    }
+
+    public function movements()
+    {
+        return $this->hasMany(InventoryMovement::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function increaseStock(int $amount,  $userId, $note = null) {
+        $this->stock()->increment('stock', $amount);
+
+        $this->movements()->create([
+            'product_id' => $this->id,
+            'user_id' => $userId,
+            'type' => 'in',
+            'quantity' => $amount,
+            'note' => $note,
+        ]);
+    }
+
+    public function decreaseStock(int $amount,  $userId, $note = null) {
+        $this->stock()->decrement('stock', $amount);
+
+        $this->movements()->create([
+            'product_id' => $this->id,
+            'user_id' => $userId,
+            'type' => 'out',
+            'quantity' => $amount,
+            'note' => $note,
+        ]);
+    }
+
+}
